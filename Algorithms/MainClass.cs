5ccc1ee7985728cad46255db39e9c1794af822b0
Andrew <chowlick@gmail.com>
2018-07-18 23:26:56 -0700

    Initial

---
+++ Algorithms/MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class MainClass
+    {
+        static string location;
+        static DateTime time;
+        static int integer;
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4 };
+
+
+
+
+
+
+            Console.WriteLine(location);
+            Console.WriteLine(integer);
+
+            Console.WriteLine(totalEvensInArray.Algorithm(array));
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ Algorithms/totalEvensInArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class totalEvensInArray
+    {
+        public static int Algorithm(int[] input)
+        {
+            int total = 0;
+
+            for (int i = 0; i <input.Length; i++)
+            {
+                if(input[i]%2==0)
+                {
+                    total += input[i];
+                }
+            }
+
+            return total;
+        }
+    }
+}
